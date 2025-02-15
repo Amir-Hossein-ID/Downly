25a6a1f61d445286cf6bb03d949a5ba0bcd55cc5
FTGUPP <52516998+FTGUPP@users.noreply.github.com>
2025-02-15 22:28:00 +0330

    async download, using semaphores

--- download.py
+++ download.py
@@ -1,38 +1,39 @@
 import requests
+import aiohttp
+import asyncio
 
 user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0'}
 
-def download(url, path):
-    r = requests.head(url, allow_redirects=True, headers=user_agent)
-    download_url = r.url
-    file_size = float('inf')
-    if not r.ok:
-        return
-    can_pause = r.headers.get('Accept-Ranges', 'none') != 'none'
-    file_size = int(r.headers.get('Content-Length', file_size))
-    chunk_size = 1024*1024
-    with open(path, 'wb') as f:
-        f.write(b'\0' * file_size)
+async def download_part(session: aiohttp.ClientSession, semaphore, url, start, end, path):
+    async with semaphore:
+        r = await session.get(url, headers={'Range': f'bytes={start}-{end}'})
+        print(f'downloaded from {start} to {end}')
+        with open(path, 'r+b') as f:
+            f.seek(start)
+            f.write(await r.read())
 
-    where = 0
-    with open(path, 'r+b') as f:
-        data = requests.get(download_url, headers={'Range': f'bytes={where}-{where + chunk_size}'})
-        while data.ok and data.content:
-            f.seek(where)
-            f.write(data.content)
-            where += int(data.headers.get('Content-Length', chunk_size))
-            if data.status_code == 200:
-                break
-            data = requests.get(download_url, headers={'Range': f'bytes={where}-{where + chunk_size}'})
-        if where == file_size:
-            print('good')
-        else:
-            print('bad')    
-    
+async def download(url, path):
+    async with aiohttp.ClientSession(headers=user_agent) as session:
+        r = await session.head(url, allow_redirects=True)
+        download_url = r.url
+        # can_pause = r.headers.get('Accept-Ranges', 'none') != 'none'
+        file_size = int(r.headers.get('Content-Length', 0))
+        chunk_size = 1024*1024*1
+        connections = 3
 
-    
+        with open(path, 'wb') as _:
+            pass
+
+        semaphore = asyncio.Semaphore(connections)
+        tasks = [
+            download_part(session, semaphore, download_url, where, where + chunk_size, path)
+            for where in range(0, file_size, chunk_size)
+        ]
+
+        await asyncio.gather(*tasks)
+
 
 if __name__ == '__main__':
     # url = input("Enter URL: ")
-    url = 'https://dl3.soft98.ir/win/AAct.4.3.1.rar'
-    download(url, 'newdownload.rar')
+    url = 'https://dl3.soft98.ir/win/Office.2013-2024.C2R.Install.v7.7.7.7.24.rar?1739645733'
+    asyncio.run(download(url, 'newdownload.rar'))
