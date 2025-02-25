d8c447e91c3dc38e40203cd8214a6e5f23267cd6
FTGUPP <52516998+FTGUPP@users.noreply.github.com>
2025-02-25 14:13:28 +0330

    ability to continue downloding previously intrrupted download

--- download.py
+++ download.py
@@ -1,10 +1,11 @@
 import aiohttp
 import aiofiles
+import aiofiles.os as aios
 from tqdm.asyncio import tqdm
 
 import asyncio
 import enum
-
+import pickle
 
 
 def human_readable_size(size_in_bytes):
@@ -36,6 +37,7 @@ class Download:
         self.n_connections = n_connections
         self.status = DownloadStatus.init
         self._head_req = None
+        self._semaphore = None
     
     async def _do_head_req(self):
         if self.session != None and not self.session.closed:
@@ -54,22 +56,55 @@ class Download:
             return int(self._head_req.headers.get('Content-Length'))
         return None
     
-    async def download_part(self, semaphore, start, end, progress_bar):
-        async with semaphore:
-            r = await self.session.get(self.url, headers={'Range': f'bytes={start}-{end}'})
+    async def is_pausable(self):
+        if not self._head_req:
+            await self._do_head_req()
+        return (await self.get_size()) != None and\
+              self._head_req.headers.get('Accept-Ranges', 'none') != 'none'
+    
+    async def _download_part(self, part, progress_bar):
+        async with self._semaphore:
+            r = await self.session.get(self.url, headers={'Range': f'bytes={part[0]}-{part[1]}'})
             async with aiofiles.open(self.path, 'r+b') as f:
-                await f.seek(start)
+                await f.seek(part[0])
                 async for data, _ in r.content.iter_chunks():
                     await f.write(data)
                     if progress_bar is not None: progress_bar.update(len(data))
-                return await f.tell() - start
+                await self._update_parts(part)
+    
+    async def _update_parts(self, downloaded_part):
+        self._parts.remove(downloaded_part)
+        if self._parts:
+            async with aiofiles.open(self.path + '.pydl', 'wb') as f:
+                await f.write(pickle.dumps(self._parts))
+        else:
+            await aios.remove(self.path + '.pydl')
+    
+    async def _remaining_parts(self):
+        size = await self.get_size()
+        if await aios.path.isfile(self.path + '.pydl'):
+            async with aiofiles.open(self.path + '.pydl', 'rb') as f:
+                data = pickle.loads(await f.read())
+                return data, size
+
+        data = [(where, where + self.chunk_size) for where in range(0, await self.get_size(), self.chunk_size)]
+        data[-1] = (data[-1][0], size)
+
+        async with aiofiles.open(self.path + '.pydl', 'wb') as f:
+            await f.write(pickle.dumps(data))
+        return data, size
     
     async def _multi_download(self, progress_bar=None):
         async with aiohttp.ClientSession(headers=user_agent) as self.session:
-            semaphore = asyncio.Semaphore(self.n_connections)
+            self._semaphore = asyncio.Semaphore(self.n_connections)
+            self._parts, size = await self._remaining_parts()
+            if progress_bar is not None:
+                a = size - sum(i[1]-i[0] for i in self._parts)
+                progress_bar.update(size - sum(i[1]-i[0] for i in self._parts))
+
             tasks = [
-                self.download_part(semaphore, where, where + self.chunk_size, progress_bar)
-                for where in range(0, await self.get_size(), self.chunk_size)
+                self._download_part(part, progress_bar)
+                for part in self._parts
             ]
 
             await asyncio.gather(*tasks)
@@ -95,20 +130,18 @@ class Download:
         file_size = await self.get_size()
 
         progress_bar = tqdm(total=file_size, ncols=70, unit="B", unit_scale=True) if progress_bar else None
-        if file_size == None:
-           await self._single_download(progress_bar)
+
+        if await self.is_pausable():
+            await self._multi_download(progress_bar)
         else:
-            self.file_size = file_size
-            if self._head_req.headers.get('Accept-Ranges', 'none') == 'none':
-                await self._single_download(progress_bar)
-            else:
-                await self._multi_download(progress_bar)
+            await self._single_download(progress_bar)
+
 
 async def main():
     url = 'https://dl3.soft98.ir/win/AAct.4.3.1.rar?1739730472'
-    url = 'https://github.com/Amir-Hossein-ID/Advent-of-Code/archive/refs/heads/master.zip'
+    # url = 'https://github.com/Amir-Hossein-ID/Advent-of-Code/archive/refs/heads/master.zip'
     # url = 'https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-s390x.sh'
-    d = Download(url, 's.rar')
+    d = Download(url, 's.rar', n_connections=2)
     download = await d.download(block=False)
     await download
 
